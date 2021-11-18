cc4b0a946108ca97e3e92b0cd8395a620a50c2f5
Pierre De Rop <pierre.derop@gmain.com>
2021-11-18 10:47:28 +0100

    testing new token

--- debug.sh
+++ debug.sh
@@ -1,4 +1,4 @@
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5006 -cp build/libs/reactor-netty-test-1.0-SNAPSHOT.jar:runtime/* test.reactor.netty.quic.Server
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5007 -cp build/libs/reactor-netty-test-1.0-SNAPSHOT.jar:runtime/* test.reactor.netty.quic.Server
 
 
 
